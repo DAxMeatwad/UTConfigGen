50899caacf3097090363f21a734aaf0b73093a85
DAxMeatwad <terryjcaulfield@gmail.com>
2016-12-10 00:27:18 -0600

    Initial commit of existing code

---
+++ test.py
@@ -0,0 +1,25 @@
+
+
+def print_menu():
+    print(30 * "-", "MENU", 30 * "-")
+    print("1. Menu Option 1")
+
+
+loop = True
+
+while loop:
+    print_menu()
+    choice = input("Enter your choice [1-5]: ")
+
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    elif choice == 5:
+        loop = False
+    else:
+        pass
