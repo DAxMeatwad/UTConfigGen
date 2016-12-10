8f753cc0a4f5514c1bf806cc5ac2da2e24c6bd9b
DAxMeatwad <terryjcaulfield@gmail.com>
2016-12-10 00:46:57 -0600

    Initial outline of loader

--- test.py
+++ test.py
@@ -1,25 +1,15 @@
+#!/usr/bin/python3
 
 
-def print_menu():
-    print(30 * "-", "MENU", 30 * "-")
-    print("1. Menu Option 1")
-
-
-loop = True
-
-while loop:
-    print_menu()
-    choice = input("Enter your choice [1-5]: ")
-
-    if choice == 1:
+class MapInfo:
+    def __init__(self, rulesetname, uniquetag, title, tooltip, description, maxplayers, minplayerstart, gamemode,
+                 gameoptions, categories, bteamgame, mapprefixes, maplist, requiredpackages):
         pass
-    elif choice == 2:
-        pass
-    elif choice == 3:
-        pass
-    elif choice == 4:
-        pass
-    elif choice == 5:
-        loop = False
-    else:
-        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
