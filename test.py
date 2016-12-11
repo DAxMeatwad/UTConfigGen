91f9ebf59593b94aff300d848f5e9897dabe36ac
DAxMeatwad <terryjcaulfield@gmail.com>
2016-12-11 13:50:46 -0600

    Initial outline of loader

--- test.py
+++ test.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python3
 
+import re
+
+gameini = r"C:\Users\terry\Desktop\LinuxServer\Rules.ini"
+
 
 class MapInfo:
-    def __init__(self, rulesetname, uniquetag, title, tooltip, description, maxplayers, minplayerstart, gamemode,
-                 gameoptions, categories, bteamgame, mapprefixes, maplist, requiredpackages):
+    def __init__(self):
+        self.utgameruleset = ""
+        self.uniquetag = ""
+        self.title = ""
+        self.tooltip = ""
+        self.description = ""
+        self.maxplayers = 0
+        self.minplayerstostart = 0
+        self.gamemode = ""
+        self.gameoptions = ""
+        self.categories = ""
+        self.bteamgame = True
+        self.mapprefixes = ""
+        self.custommaplist = []
+        self.requiredpackages = []
+
+
+def getmapinfo():
+    with open(gameini, "r") as f:
+        for line in f:
+            print(line.splitlines())
+
+    def setmapinfo(self):
         pass
 
 
 def main():
-    pass
-
+    getmapinfo()
 
 if __name__ == '__main__':
     main()
